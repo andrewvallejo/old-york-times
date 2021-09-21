177329a3914e9a5a9aab6b7b024f6698d52e4962
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-21 11:16:27 -0600

    Add API data cleaner

--- src/utility/apiCalls/apiCleaner.js
+++ src/utility/apiCalls/apiCleaner.js
@@ -1,14 +1,17 @@
 export const cleanNews = ({ results }) => {
-	return results.map((news) => {
-		return {
-			abstract: news.abstract,
-			caption:  news.byline,
-			createdDate: news.created_date ,
-			tags: news.des_facet ,
-			images: news.multimedia ,
-			section: news.section ,
-			url: news.short_url ,
-			title: news.title ,
-		}
-	})
+	if (results) {
+		return results.map((news) => {
+			return {
+				abstract: news.abstract,
+				caption:  news.byline,
+				createdDate: news.created_date ,
+				tags: news.des_facet ,
+				images: news.multimedia ,
+				section: news.section ,
+				url: news.short_url ,
+				title: news.title ,
+			}
+		})
+	}
+	console.error('Server Error: No news available!')
 }
