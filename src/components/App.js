5300f88ca7e843c6c835d152620c975b0e7a0b46
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-21 11:15:08 -0600

    Refactor App to have context

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Route, Switch } from 'react-router';
+import { reducer } from '../utility/reducer'
+import { UserContext } from '../utility/UserContext';
 import { HomePage } from '../pages/HomePage'
 
+const initialState = {
+  news: [],
+  loaded: false
+}
+
 export const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <main>
-      <Switch>
-        <Route path='/' component={HomePage} />
-      </Switch>
-    </main>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <main>
+        <Switch>
+          <Route path='/' component={HomePage} />
+        </Switch>
+      </main>
+
+    </UserContext.Provider>
   );
 }
 
