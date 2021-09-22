8f8246c802af635ed88a8108d12087f4a6bb2822
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-22 17:25:24 -0600

    Update all CSS for header and navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,15 @@
+import { SearchBar } from "./SearchBar"
+
 export const NavigationBar = () => {
 
 	return (
-		<nav>
-			<ul className='navigation'>
-				<li>Home</li>
-				<li>Filter</li>
+		<section className='navbar'>
+			<ul className='nav-links'>
+				<li className='home link'>Home</li>
+				<li className='filter link'>Filter</li>
 			</ul>
-			<h2 >New York, Monday, September 21, 2021</h2>	
-		</nav>
+			<h2 className='today'>New York, Monday, September 21, 2021</h2>	
+			<SearchBar />
+		</section>
 	)
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,8 @@
 export const SearchBar = () => {
-	
+	return (
+		<form className='search-bar'>
+				<input className='search-input' type='text' placeholder='Search...' />
+				<button className='search-button'>🔍</button>
+		</form>
+	)				
 }
