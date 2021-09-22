327cb94fd2db41c6702c8130f602aa8b82b9fb63
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-22 11:48:37 -0600

    Add basic skeleton for Header elements

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+export const NavigationBar = () => {
+
+	return (
+		<nav>
+			<ul className='navigation'>
+				<li>Home</li>
+				<li>Filter</li>
+			</ul>
+			<h2 >New York, Monday, September 21, 2021</h2>	
+		</nav>
+	)
+}
