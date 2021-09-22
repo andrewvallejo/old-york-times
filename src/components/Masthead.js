4e16fb4a4f3c905c75d619906336481db702f2bb
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-22 14:45:19 -0600

    Add JSX for header

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -1,10 +1,16 @@
 export const Masthead = () => {
 
 	return (
-		<header>
-			<aside></aside>
-			<h1>The Old York Times</h1>
-			<aside></aside>
+		<header className='masthead'>
+			<aside className='slogan'>
+				<p className='top-slogan'>"All the News</p>
+				<p className='bottom-slogan'>"That's Fit to Print"</p>
+			</aside>
+			<h1 className='title'>The Old York Times</h1>
+			<aside className='edition'>
+				<h3 className='edition-title'>Digital Paper Edition</h3>
+				<p className='information'>This website was designed to help those who want that old-timey feeling of reading a real newspaper without wasting actual paper.</p>
+			</aside>
 		</header>
 	)
 }
