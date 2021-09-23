c637f312b4d4c1ee648d181fd39b8b260376133d
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-23 10:00:22 -0600

    Remove count

--- src/utility/apiCalls/apiCleaner.js
+++ src/utility/apiCalls/apiCleaner.js
@@ -1,21 +1,16 @@
 export const cleanNews = ({ results }) => {
+
 	if (results) {
-		let count = 0
 		return results.map((news) => {
-			count++
-			if(count <= 8) {
-				return {
-					abstract: news.abstract,
-					byline:  news.byline,
-					date: news.created_date ,
-					tags: news.des_facet ,
-					images: news.multimedia ,
-					section: news.section ,
-					url: news.short_url ,
-					title: news.title ,
-					} 
-			} else {
-				return {}
+			return {
+				abstract: news.abstract,
+				byline: news.byline,
+				date: news.created_date,
+				tags: news.des_facet,
+				images: news.multimedia,
+				section: news.section,
+				url: news.short_url,
+				title: news.title,
 			}
 		})
 	}
