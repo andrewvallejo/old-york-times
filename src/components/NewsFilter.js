6bac1106d4914b401e39050b1f3bc09dec85b255
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-23 17:31:16 -0600

    Add ability to use form value to update state

--- src/components/NewsFilter.js
+++ src/components/NewsFilter.js
@@ -1,34 +1,46 @@
+import { useContext } from 'react'
+import { UserContext } from '../utility/UserContext'
+
 export const NewsFilter = () => {
+	const {state, dispatch} = useContext(UserContext)	
+
+const handleClick = (event) => {
+	event.preventDefault()
+	const section = event.target.value
+	console.log(state)
+	dispatch({state, action: {type:'SETSECTION', value: section}})
+}
+
 	return (
 		<form>
 		<label htmlFor='sections'>
-				<select className='filter-list' name="sections" id="sections">
-					<option selected disabled>Filter</option>
-					<option value="home">All</option>
-					<option value="arts">Arts</option>
-					<option value="automobiles">Automobiles</option>
-					<option value="books">Books</option>
-					<option value="fashion">Fashion</option>
-					<option value="food">Food</option>
-					<option value="health">Health</option>
-					<option value="insider">Insider</option>
-					<option value="magazine">Magazine</option>
-					<option value="movies">Movies</option>
-					<option value="nyregion">NY Region</option>
-					<option value="obituaries">Obituaries</option>
-					<option value="opinion">Opinion</option>
-					<option value="politics">Politics</option>
-					<option value="realestate">Real Estate</option>
-					<option value="science">Science</option>
-					<option value="sport">Sport</option>
-					<option value="sundayreview">Sunday Review</option>
-					<option value="technology">Technology</option>
-					<option value="theater">Theater</option>
-					<option value="t-magazine">T-Magazine</option>
-					<option value="travel">Travel</option>
-					<option value="upshot">Upshot</option>
-					<option value="us">US</option>
-					<option value="world">World</option>
+				<select className='filter-list' name='sections' id='sections' onChange={(e) => handleClick(e)}>
+					<option value=''>Filter</option>
+					<option value='home'>All</option>
+					<option value='arts'>Arts</option>
+					<option value='automobiles'>Automobiles</option>
+					<option value='books'>Books</option>
+					<option value='fashion'>Fashion</option>
+					<option value='food'>Food</option>
+					<option value='health'>Health</option>
+					<option value='insider'>Insider</option>
+					<option value='magazine'>Magazine</option>
+					<option value='movies'>Movies</option>
+					<option value='nyregion'>NY Region</option>
+					<option value='obituaries'>Obituaries</option>
+					<option value='opinion'>Opinion</option>
+					<option value='politics'>Politics</option>
+					<option value='realestate'>Real Estate</option>
+					<option value='science'>Science</option>
+					<option value='sport'>Sport</option>
+					<option value='sundayreview'>Sunday Review</option>
+					<option value='technology'>Technology</option>
+					<option value='theater'>Theater</option>
+					<option value='t-magazine'>T-Magazine</option>
+					<option value='travel'>Travel</option>
+					<option value='upshot'>Upshot</option>
+					<option value='us'>US</option>
+					<option value='world'>World</option>
 				</select>
 		</label>
 			</form>
