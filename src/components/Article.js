5cee6e494f7fa9f2dfb86fde7bc1ab488c816d3e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-23 12:12:49 -0600

    Add class names to elements

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,9 @@ export const Article = ({ content }) => {
 	
 	return (
 		<article className='article'>
-			<h4> {title} </h4>
-			<h5> {byline} </h5>
-			<button>Read More</button>
+			<h4 className='title'> {title} </h4>
+			<h5 className='byline'> {byline} </h5>
+			<button className='read-button'>Read More</button>
 		</article>
 	)
 }
