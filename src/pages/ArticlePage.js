5fe46fcfe0176a8966fd856b2874e70b8f15435f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-24 09:05:49 -0600

    Add basic logic to reveal bare minimum

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,17 +1,24 @@
 import { useContext } from "react"
+import { formatDate } from "../utility/formatDate"
 import { UserContext } from "../utility/UserContext"
 
 export const ArticlePage = () => {
 	const { state } = useContext(UserContext)
-	const { abstract, byline, date, tags, images, section, url, title } = state.article
+	const { abstract, byline, date, url, title } = state.article
+
+	const updatedDate = formatDate(date)
 
 	return (
 		<section className='news-article'>
-			{abstract}
-			{byline}
-			{date}
-			{title}
-			<a href={`${url}`}>Read more</a>
+			<header>
+				<h2>{title}</h2>
+				<div className='author-container'>
+					<h3>{byline} </h3>
+					<p>{updatedDate}</p>
+				</div>
+			</header>
+			<p className='abstract'>{abstract}</p>
+			<a href={`${url}`}>Read More over at the New York Times</a>
 		</section>
 	)
 }
