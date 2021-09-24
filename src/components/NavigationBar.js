c593a55077be80a93ea7c669689211088ed16084
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-24 09:05:22 -0600

    Add conditional to reveal filter memu

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,15 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import { NewsFilter } from "./NewsFilter"
 import { SearchBar } from "./SearchBar"
 
 export const NavigationBar = () => {
+	const location = useLocation().pathname
 
 	return (
 		<section className='navbar'>
 			<nav className='nav-links'>
 				<NavLink to='/' className='home link'>Home</NavLink>
-				<NewsFilter /> 
+				{!location === '/' && <NewsFilter />}
 			</nav>
 			<h2 className='today'>New York, Friday, September 24, 2021</h2>
 			<SearchBar />
