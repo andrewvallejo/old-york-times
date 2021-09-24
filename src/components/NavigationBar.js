0423cb8a939643e6a67a7a86dde0fa48a2fe95a0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-24 10:27:50 -0600

    Fix filter HOTFIX

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,15 +1,14 @@
-import { NavLink, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { NewsFilter } from "./NewsFilter"
 import { SearchBar } from "./SearchBar"
 
 export const NavigationBar = () => {
-	const location = useLocation().pathname
 
 	return (
 		<section className='navbar'>
 			<nav className='nav-links'>
 				<NavLink to='/' className='home link'>Home</NavLink>
-				{!location === '/' && <NewsFilter />}
+				<NewsFilter />
 			</nav>
 			<h2 className='today'>New York, Friday, September 24, 2021</h2>
 			<SearchBar />
