b5f3dedc95d5937aaf6a86e7dd848744cad3b20b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-23 20:07:31 -0600

    Add rerouting logic to goto full article view

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,18 @@
+import { useHistory } from "react-router"
+
 export const Article = ({ content }) => {
 	const { title, byline } = content
-	
+const history = useHistory()
+const pathway = title.toString().split(' ').join('-')
+	const handleClick = () => {
+		history.push(`/article/${pathway}`)
+	}
+
 	return (
 		<article className='article'>
 			<h2 className='title'> {title} </h2>
 			<h3 className='byline'> {byline} </h3>
-			<button className='read-button'>Read More</button>
+			<button className='read-button' onClick={() => handleClick()}>Read More</button>
 		</article>
 	)
 }
