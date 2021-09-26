b776a5046088fe36dcb6261dd4115a271219fc3a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-26 15:14:08 -0600

    Add function to give articles a different class

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { useHistory } from "react-router"
 import { UserContext } from "../utility/UserContext"
 
-export const Article = ({ content, id }) => {
+export const Article = ({ content }) => {
 	const { state, dispatch } = useContext(UserContext)
 	const { title, byline } = content
 	const history = useHistory()
@@ -13,8 +13,24 @@ export const Article = ({ content, id }) => {
 		dispatch({ state, action: { type: 'SETARTICLE', value: content } })
 	}
 
+	const newsShape = (id) => {
+		id = parseInt(id.toString().split('').pop())
+		switch (id) {
+			case 0: case 3: case 6: case 9:
+			return 'longways'
+			case 1: case 4: case 7: case 8:
+			return 'sideways'
+			case 2: case 5:
+			return 'square'
+			default:
+				break;
+		}
+	}
+
+
+
 	return (
-		<article className='article'>
+		<article className={`article ${newsShape(content.id)}`}>
 			<h2 className='title'> {title} </h2>
 			<h3 className='byline'> {byline} </h3>
 			<button className='read-button' onClick={() => handleClick()}>Read More</button>
