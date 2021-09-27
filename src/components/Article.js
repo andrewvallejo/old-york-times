2b8023f7fe46e8ea6b13f358149f636608c6b2c2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-26 20:09:35 -0600

    Update grid to to dynamic with different shapes

--- src/components/Article.js
+++ src/components/Article.js
@@ -7,6 +7,7 @@ export const Article = ({ content }) => {
 	const { title, byline } = content
 	const history = useHistory()
 	const pathway = title.toString().split(' ').join('-').replace(/['":.]+/g, '',)
+	let image
 
 	const handleClick = () => {
 		history.push(`/article/${pathway}`)
@@ -14,11 +15,16 @@ export const Article = ({ content }) => {
 	}
 
 	const newsShape = (id) => {
+		const url = content.images[0].url
 		id = parseInt(id.toString().split('').pop())
+
 		switch (id) {
 			case 0: case 3: case 6: case 9:
+				image = <img  className='image' src={url} alt='Article'/>
+
 			return 'longways'
 			case 1: case 4: case 7: case 8:
+				
 			return 'sideways'
 			case 2: case 5:
 			return 'square'
@@ -31,6 +37,7 @@ export const Article = ({ content }) => {
 
 	return (
 		<article className={`article ${newsShape(content.id)}`}>
+			{image}
 			<h2 className='title'> {title} </h2>
 			<h3 className='byline'> {byline} </h3>
 			<button className='read-button' onClick={() => handleClick()}>Read More</button>
